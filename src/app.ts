aa9c8574ced3fa7ffb3e5af87f2c92dd603e53ec
Ronald Huggler <ronald@huggler.com.br>
2023-06-05 16:00:28 -0300

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Express from 'express'
 import { leaderboardsRouter } from './routes'
 import { authMiddleware } from './auth'
+import cors from 'cors'
 
 export const app = Express()
 
+app.use(cors())
 app.use(authMiddleware)
 app.use(leaderboardsRouter)
